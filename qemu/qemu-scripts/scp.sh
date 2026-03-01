ed9eba9e3b19e07eb9367d4a05515df45d5268d8
Mimo <lubbarbarek@gmail.com>
2026-03-01 11:47:04 +0100

    Reinitialized repo

--- qemu/qemu-scripts/scp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# vim: fdm=marker
-
-. `dirname $0`/default_config.sh
-
-scp -p $NET1_SSH_TO_VM_PORT localhost $@
-
--- qemu/qemu-scripts/watch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# vim: fdm=marker
-
-. `dirname $0`/default_config.sh
-
-remote-viewer -f --hotkeys=toggle-fullscreen=shift+f11 spice://${SPICE_HOST}:${SPICE_PORT}/ -f
-
